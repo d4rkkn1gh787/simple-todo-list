a1f5c50d222af07d84cb20b55e10a8539bb85c5b
Frank Gonzalez <frank.gonzalez628@gmail.com>
2016-08-29 08:53:33 -0500

    remove and edit working

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ formBody.setAttribute("method", "get");
 var bar = document.createElement('input');
 bar.setAttribute("type", "text");
 bar.id = "textbar";
+bar.setAttribute('placeholder', "Add task here");
 formBody.appendChild(bar)
 
 // Add Button
@@ -28,18 +29,50 @@ var list = document.createElement('ul');
 document.body.appendChild(list);
 
 // Functionality
+function listItem() {
+    event.preventDefault();
+    var text = document.getElementById('textbar').value;
+    var addedItem = document.createElement('li');
+    var removeButton = document.createElement('button')
+    var edit = document.createElement('button')
+
+    addedItem.innerHTML = text;
+    removeButton.setAttribute('type', 'button');
+    removeButton.className = "rButton";
+    removeButton.innerHTML = "X";
+    edit.innerHTML = "Edit";
+    edit.className = "edit"
+
+    list.appendChild(addedItem);
+    addedItem.appendChild(removeButton);
+    addedItem.appendChild(edit);
+
+    document.getElementById('textbar').value = "";
+
+    removeButton.addEventListener("click", function() {
+        addedItem.parentNode.removeChild(addedItem)
+    })
+
+    edit.addEventListener('click', function() {
+        var editText = "<input type='text' id='editText' value=" + text + "><button id='update' type='submit'>Update</button>";
+
+        addedItem.innerHTML = editText;
+        var update = document.getElementById("update")
+        update.addEventListener("click", function() {
+            text = document.getElementById("editText").value;
+            addedItem.innerHTML = text;
+            addedItem.appendChild(edit);
+            addedItem.appendChild(removeButton)
+
+        })
+    })
+}
+
+
 addButton.addEventListener('click', function(event) {
     if (document.getElementById("textbar").value !== "") {
-        event.preventDefault();
-        var text = document.getElementById('textbar').value;
-        var addedItem = document.createElement('li');
-        addedItem.className = "items";
-        addedItem.innerHTML = text;
-        list.appendChild(addedItem);
-        document.getElementById('textbar').value = "";
+        listItem();
     } else {
         event.preventDefault();
     }
-
-
 })
